af22f602e6073d877c90dfd4b5aee7b37a6ee98a
Daniel Cortes <daniel.cortes.83944@gmail.com>
2021-08-22 20:40:51 -0500

    feat: Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Job } from './job'
+import { JobService } from './job.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'capturefront';
+export class AppComponent implements OnInit{
+  public jobs: Job[];
+
+  constructor(private jobService: JobService){}
+
+  ngOnInit(){
+    this.getJobs()
+  }
+
+  public getJobs(): void {
+    this.jobService.getAllJobs().subscribe(
+      (response: Job[]) => {
+        this.jobs = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message)
+      }
+    )
+  }
 }
--- src/app/job.service.ts
+++ src/app/job.service.ts
@@ -1,16 +1,37 @@
+// import { Injectable } from '@angular/core';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class JobService {
+
+//   constructor() { }
+// }
+
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs'
+import { HttpClient } from '@angular/common/http'
+import { Job } from './job'
+import { environment } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 
 export class JobService {
-    private apiServerUrl = '';
+    private apiServerUrl = environment.apiBaseUrl;
 
     constructor(private http: HttpClient) { }
 
-    public getAllJobs(): Observable<any> {
+    public getAllJobs(): Observable<Job[]> {
         return this.http.get<any>(`${this.apiServerUrl}/jobs`)
+    }
+
+    public addJob(job: Job): Observable<Job> {
+        return this.http.post<Job>(`${this.apiServerUrl}/addJob`, job)
+    }
+
+    public getJobById(jobId: number): Observable<Job> {
+        return this.http.get<Job>(`${this.apiServerUrl}/find/${jobId}`)
     }
 }
